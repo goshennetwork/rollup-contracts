96dc9fd3f7d23fdfc14d11bfb17aee614a4955aa
laizy <aochyi@126.com>
2022-02-09 17:34:09 +0800

    add bytes slice utils, rlp reader/writer (#5)

---
+++ contracts/libraries/RLPCodec.t.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./RLPReader.sol";
+import "./RLPWriter.sol";
+import "./BytesSlice.sol";
+
+contract RLPCodecTest {
+    function testReadWriteUint(uint256 val) public {
+        bytes memory rlp = RLPWriter.writeUint(val);
+        uint256 decoded = RLPReader.readUint256(rlp);
+        require(decoded == val);
+    }
+
+    function checkWriteUint(uint256 val, bytes memory encoded) internal {
+        require(BytesSlice.equal(RLPWriter.writeUint(val), encoded));
+        uint256 decoded = RLPReader.readUint256(encoded);
+        require(decoded == val);
+    }
+
+    function checkBool(bool val, bytes memory encoded) internal {
+        require(BytesSlice.equal(RLPWriter.writeBool(val), encoded));
+        bool decoded = RLPReader.readBool(encoded);
+        require(decoded == val);
+    }
+
+    function testWrite() public {
+        checkWriteUint(0, hex"80");
+        checkWriteUint(1, hex"01");
+        checkWriteUint(0, hex"80");
+        checkWriteUint(0, hex"80");
+        checkWriteUint(1, hex"01");
+        checkWriteUint(127, hex"7F");
+        checkWriteUint(128, hex"8180");
+        checkWriteUint(256, hex"820100");
+        checkWriteUint(1024, hex"820400");
+        checkWriteUint(0xFFFFFF, hex"83FFFFFF");
+        checkWriteUint(0xFFFFFFFF, hex"84FFFFFFFF");
+        checkWriteUint(0xFFFFFFFF, hex"84FFFFFFFF");
+        checkWriteUint(0xFFFFFFFFFF, hex"85FFFFFFFFFF");
+        checkWriteUint(0xFFFFFFFFFFFF, hex"86FFFFFFFFFFFF");
+        checkWriteUint(0xFFFFFFFFFFFFFF, hex"87FFFFFFFFFFFFFF");
+        checkWriteUint(0xFFFFFFFFFFFFFFFF, hex"88FFFFFFFFFFFFFFFF");
+
+        checkBool(true, hex"01");
+        checkBool(false, hex"80");
+
+        checkWriteBytes("", hex"80");
+        checkWriteBytes(hex"00", hex"00");
+        checkWriteBytes(hex"01", hex"01");
+        checkWriteBytes(hex"7f", hex"7f");
+        checkWriteBytes(hex"80", hex"8180");
+        checkWriteBytes(hex"ff", hex"81ff");
+        checkWriteBytes(hex"010203", hex"83010203");
+        checkWriteBytes("dog", hex"83646F67");
+        checkWriteBytes(
+            "Lorem ipsum dolor sit amet, consectetur adipisicing eli",
+            hex"B74C6F72656D20697073756D20646F6C6F722073697420616D65742C20636F6E7365637465747572206164697069736963696E6720656C69"
+        );
+        checkWriteBytes(
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+            hex"B8384C6F72656D20697073756D20646F6C6F722073697420616D65742C20636F6E7365637465747572206164697069736963696E6720656C6974"
+        );
+        checkWriteBytes(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mauris magna, suscipit sed vehicula non, iaculis faucibus tortor. Proin suscipit ultricies malesuada. Duis tortor elit, dictum quis tristique eu, ultrices at risus. Morbi a est imperdiet mi ullamcorper aliquet suscipit nec lorem. Aenean quis leo mollis, vulputate elit varius, consequat enim. Nulla ultrices turpis justo, et posuere urna consectetur nec. Proin non convallis metus. Donec tempor ipsum in mauris congue sollicitudin. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse convallis sem vel massa faucibus, eget lacinia lacus tempor. Nulla quis ultricies purus. Proin auctor rhoncus nibh condimentum mollis. Aliquam consequat enim at metus luctus, a eleifend purus egestas. Curabitur at nibh metus. Nam bibendum, neque at auctor tristique, lorem libero aliquet arcu, non interdum tellus lectus sit amet eros. Cras rhoncus, metus ac ornare cursus, dolor justo ultrices metus, at ullamcorper volutpat",
+            hex"B904004C6F72656D20697073756D20646F6C6F722073697420616D65742C20636F6E73656374657475722061646970697363696E6720656C69742E20437572616269747572206D6175726973206D61676E612C20737573636970697420736564207665686963756C61206E6F6E2C20696163756C697320666175636962757320746F72746F722E2050726F696E20737573636970697420756C74726963696573206D616C6573756164612E204475697320746F72746F7220656C69742C2064696374756D2071756973207472697374697175652065752C20756C7472696365732061742072697375732E204D6F72626920612065737420696D70657264696574206D6920756C6C616D636F7270657220616C6971756574207375736369706974206E6563206C6F72656D2E2041656E65616E2071756973206C656F206D6F6C6C69732C2076756C70757461746520656C6974207661726975732C20636F6E73657175617420656E696D2E204E756C6C6120756C74726963657320747572706973206A7573746F2C20657420706F73756572652075726E6120636F6E7365637465747572206E65632E2050726F696E206E6F6E20636F6E76616C6C6973206D657475732E20446F6E65632074656D706F7220697073756D20696E206D617572697320636F6E67756520736F6C6C696369747564696E2E20566573746962756C756D20616E746520697073756D207072696D697320696E206661756369627573206F726369206C756374757320657420756C74726963657320706F737565726520637562696C69612043757261653B2053757370656E646973736520636F6E76616C6C69732073656D2076656C206D617373612066617563696275732C2065676574206C6163696E6961206C616375732074656D706F722E204E756C6C61207175697320756C747269636965732070757275732E2050726F696E20617563746F722072686F6E637573206E69626820636F6E64696D656E74756D206D6F6C6C69732E20416C697175616D20636F6E73657175617420656E696D206174206D65747573206C75637475732C206120656C656966656E6420707572757320656765737461732E20437572616269747572206174206E696268206D657475732E204E616D20626962656E64756D2C206E6571756520617420617563746F72207472697374697175652C206C6F72656D206C696265726F20616C697175657420617263752C206E6F6E20696E74657264756D2074656C6C7573206C65637475732073697420616D65742065726F732E20437261732072686F6E6375732C206D65747573206163206F726E617265206375727375732C20646F6C6F72206A7573746F20756C747269636573206D657475732C20617420756C6C616D636F7270657220766F6C7574706174"
+        );
+    }
+
+    function testWriteList() public {
+        bytes[] memory list = new bytes[](15);
+        list[0] = "aaa";
+        list[1] = "bbb";
+        list[2] = "ccc";
+        list[3] = "ddd";
+        list[4] = "eee";
+        list[5] = "fff";
+        list[6] = "ggg";
+        list[7] = "hhh";
+        list[8] = "iii";
+        list[9] = "jjj";
+        list[10] = "kkk";
+        list[11] = "lll";
+        list[12] = "mmm";
+        list[13] = "nnn";
+        list[14] = "ooo";
+        bytes[] memory encodedList = new bytes[](15);
+        for (uint256 i = 0; i < list.length; i++) {
+            encodedList[i] = RLPWriter.writeBytes(list[i]);
+        }
+        require(
+            BytesSlice.equal(
+                RLPWriter.writeList(encodedList),
+                hex"F83C836161618362626283636363836464648365656583666666836767678368686883696969836A6A6A836B6B6B836C6C6C836D6D6D836E6E6E836F6F6F"
+            )
+        );
+    }
+
+    function checkWriteBytes(bytes memory val, bytes memory encoded) internal {
+        require(BytesSlice.equal(RLPWriter.writeBytes(val), encoded));
+        bytes memory decoded = RLPReader.readBytes(encoded);
+        require(BytesSlice.equal(decoded, val));
+    }
+}
---
+++ contracts/libraries/RLPReader.sol
@@ -0,0 +1,227 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./BytesSlice.sol";
+
+/**
+ * @title RLPReader
+ * @dev Adapted from "RLPReader" by Hamdi Allam (hamdi.allam97@gmail.com) and Optimism
+ */
+library RLPReader {
+    uint256 internal constant MAX_LIST_LENGTH = 32;
+    enum RLPItemType {
+        DATA_ITEM,
+        LIST_ITEM
+    }
+
+    /**
+     * Reads an RLP list value into a list of RLP items.
+     * @param rawRlp RLP list value.
+     * @return Decoded RLP list items.
+     */
+    function readList(Slice memory rawRlp) internal pure returns (Slice[] memory) {
+        (uint256 listOffset, , RLPItemType itemType) = decodeKind(rawRlp);
+
+        require(itemType == RLPItemType.LIST_ITEM, "Invalid RLP list value.");
+
+        // Solidity in-memory arrays can't be increased in size, but *can* be decreased in size by
+        // writing to the length. Since we can't know the number of RLP items without looping over
+        // the entire input, we'd have to loop twice to accurately size this array. It's easier to
+        // simply set a reasonable maximum list length and decrease the size before we finish.
+        Slice[] memory out = new Slice[](MAX_LIST_LENGTH);
+
+        uint256 itemCount = 0;
+        uint256 offset = listOffset;
+        while (offset < rawRlp.len) {
+            require(itemCount < MAX_LIST_LENGTH, "Provided RLP list exceeds max list length.");
+
+            (uint256 itemOffset, uint256 itemLength, ) = decodeKind(
+                Slice({ len: rawRlp.len - offset, ptr: rawRlp.ptr + offset })
+            );
+
+            out[itemCount] = Slice({ len: itemLength + itemOffset, ptr: rawRlp.ptr + offset });
+
+            itemCount += 1;
+            offset += itemOffset + itemLength;
+        }
+
+        // Decrease the array size to match the actual item count.
+        assembly {
+            mstore(out, itemCount)
+        }
+
+        return out;
+    }
+
+    /**
+     * Reads an RLP list value into a list of RLP items.
+     * @param rawRlp RLP list value.
+     * @return Decoded RLP list items.
+     */
+    function readList(bytes memory rawRlp) internal pure returns (Slice[] memory) {
+        return readList(BytesSlice.fromBytes(rawRlp));
+    }
+
+    function readBytes(Slice memory rawRlp) internal pure returns (bytes memory) {
+        (uint256 itemOffset, uint256 itemLength, RLPItemType itemType) = decodeKind(rawRlp);
+
+        require(itemType == RLPItemType.DATA_ITEM, "Invalid RLP bytes value.");
+
+        return BytesSlice.toBytes(Slice({ ptr: rawRlp.ptr + itemOffset, len: itemLength }));
+    }
+
+    function readBytes(bytes memory rawRlp) internal pure returns (bytes memory) {
+        return readBytes(BytesSlice.fromBytes(rawRlp));
+    }
+
+    function readString(Slice memory rawRlp) internal pure returns (string memory) {
+        return string(readBytes(rawRlp));
+    }
+
+    function readString(bytes memory rawRlp) internal pure returns (string memory) {
+        return readString(BytesSlice.fromBytes(rawRlp));
+    }
+
+    function readUint256(Slice memory rawRlp) internal pure returns (uint256) {
+        require(rawRlp.len <= 33, "Invalid RLP bytes32 value.");
+
+        (uint256 itemOffset, uint256 itemLength, RLPItemType itemType) = decodeKind(rawRlp);
+
+        require(itemType == RLPItemType.DATA_ITEM, "Invalid RLP bytes32 value.");
+
+        uint256 ptr = rawRlp.ptr + itemOffset;
+        uint256 out;
+        assembly {
+            out := mload(ptr)
+
+            // Shift the bytes over to match the item size.
+            if lt(itemLength, 32) {
+                out := div(out, exp(256, sub(32, itemLength)))
+            }
+        }
+
+        return out;
+    }
+
+    function readUint256(bytes memory rawRlp) internal pure returns (uint256) {
+        return readUint256(BytesSlice.fromBytes(rawRlp));
+    }
+
+    function readBytes32(bytes memory rawRlp) internal pure returns (bytes32) {
+        return readBytes32(BytesSlice.fromBytes(rawRlp));
+    }
+
+    function readBytes32(Slice memory rawRlp) internal pure returns (bytes32) {
+        return bytes32(readUint256(rawRlp));
+    }
+
+    function readBool(Slice memory rawRlp) internal pure returns (bool) {
+        require(rawRlp.len == 1, "Invalid RLP boolean value.");
+
+        uint256 ptr = rawRlp.ptr;
+        uint256 out;
+        assembly {
+            out := byte(0, mload(ptr))
+        }
+
+        require(out == 0x80 || out == 1, "Invalid RLP boolean value");
+        return out == 1;
+    }
+
+    function readBool(bytes memory rawRlp) internal pure returns (bool) {
+        return readBool(BytesSlice.fromBytes(rawRlp));
+    }
+
+    /**
+     * Like readAddress, but if the length is 1, and value is (false)"80", return empty address
+     */
+    function readOptionAddress(Slice memory rawRlp) internal pure returns (address) {
+        if (rawRlp.len == 1 && readBool(rawRlp) == false) {
+            return address(0);
+        }
+        require(rawRlp.len == 21, "Invalid RLP address value.");
+
+        return address(uint160(readUint256(rawRlp)));
+    }
+
+    function readOptionAddress(bytes memory rawRlp) internal pure returns (address) {
+        return readOptionAddress(BytesSlice.fromBytes(rawRlp));
+    }
+
+    function readAddress(Slice memory rawRlp) internal pure returns (address) {
+        require(rawRlp.len == 21, "Invalid RLP address value.");
+
+        return address(uint160(readUint256(rawRlp)));
+    }
+
+    function readAddress(bytes memory rawRlp) internal pure returns (address) {
+        return readAddress(BytesSlice.fromBytes(rawRlp));
+    }
+
+    /**
+     * Decodes the length and item type of an RLP item.
+     * @param rawRlp RLP item to decode.
+     * @return Start offset of the encoded data.
+     * @return Length of the encoded data.
+     * @return RLP item type (LIST_ITEM or DATA_ITEM).
+     */
+    function decodeKind(Slice memory rawRlp)
+        internal
+        pure
+        returns (
+            uint256,
+            uint256,
+            RLPItemType
+        )
+    {
+        require(rawRlp.len > 0, "RLP item cannot be null.");
+
+        uint256 ptr = rawRlp.ptr;
+        uint256 prefix;
+        assembly {
+            prefix := byte(0, mload(ptr))
+        }
+
+        if (prefix <= 0x7f) {
+            // Single byte.
+            return (0, 1, RLPItemType.DATA_ITEM);
+        } else if (prefix <= 0xb7) {
+            // Short string.
+            uint256 strLen = prefix - 0x80;
+            require(rawRlp.len > strLen, "Invalid RLP short string.");
+            return (1, strLen, RLPItemType.DATA_ITEM);
+        } else if (prefix <= 0xbf) {
+            // Long string.
+            uint256 lenOfStrLen = prefix - 0xb7;
+            require(rawRlp.len > lenOfStrLen, "Invalid RLP long string length.");
+            uint256 strLen;
+            assembly {
+                // Pick out the string length. note: rlp的标准要求整数采用大端编码，且必须移除前缀0,因此这里没有做这个检查.
+                strLen := div(mload(add(ptr, 1)), exp(256, sub(32, lenOfStrLen)))
+            }
+            require(rawRlp.len > lenOfStrLen + strLen, "Invalid RLP long string.");
+
+            return (1 + lenOfStrLen, strLen, RLPItemType.DATA_ITEM);
+        } else if (prefix <= 0xf7) {
+            // Short list.
+            uint256 listLen = prefix - 0xc0;
+            require(rawRlp.len > listLen, "Invalid RLP short list.");
+
+            return (1, listLen, RLPItemType.LIST_ITEM);
+        } else {
+            // Long list.
+            uint256 lenOfListLen = prefix - 0xf7;
+            require(rawRlp.len > lenOfListLen, "Invalid RLP long list length.");
+
+            uint256 listLen;
+            assembly {
+                // Pick out the list length. note: 同上.
+                listLen := div(mload(add(ptr, 1)), exp(256, sub(32, lenOfListLen)))
+            }
+
+            require(rawRlp.len > lenOfListLen + listLen, "Invalid RLP long list.");
+
+            return (1 + lenOfListLen, listLen, RLPItemType.LIST_ITEM);
+        }
+    }
+}
