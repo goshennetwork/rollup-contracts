f93b01190bbc82d6ec2fb4270354e70613a6ed8f
XuDon9 Liu AKA Unknown Rapper Of CN background diablo & revelations, a man optimistic, nagetive, silent, active, Taoism <1027309561@qq.com>
2022-02-09 18:32:17 +0800

    merkle test

---
+++ contracts/merkle/MerkleCodec.t.sol
@@ -0,0 +1,9 @@
+pragma solidity >0.5.0 <0.8.0;
+
+contract MerkleCodecTest {
+    /*because this merkle is used to state machine, so it's only need 2 method: get specific data of path
+    and update the new trie root when leaf value is changed
+    */
+
+    function testHaa() public {}
+}
