1f966ab9ef01352f4bd6ba644d2d625f86da95c1
XuDon9 Liu AKA Unknown Rapper Of CN background diablo & revelations, a man optimistic, nagetive, silent, active, Taoism <1027309561@qq.com>
2022-02-16 18:58:21 +0800

    -

--- .gha.precheck.sh
+++ .gha.precheck.sh
@@ -7,7 +7,6 @@ if [ $RUNNER_OS == 'Linux' ]; then
   GOPRIVATE=github.com/ontology-layer-2 go get github.com/ontology-layer-2/go-ethereum@contract_fixed_trie
   bash ./.gha.gofmt.sh
   bash ./.gha.gotest.sh
-  bash ./build.sh
 elif [ $RUNNER_OS == 'osx' ]; then
   echo "osx sys"
   ./build.sh
