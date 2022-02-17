8c418df7fc07eef3ebf53540c4be6c4cba563a40
XuDon9 Liu AKA Unknown Rapper Of CN background diablo & revelations, a man optimistic, nagetive, silent, active, Taoism <1027309561@qq.com>
2022-02-17 15:10:28 +0800

    use branch

--- .gha.precheck.sh
+++ .gha.precheck.sh
@@ -5,8 +5,7 @@ VERSION=$(git describe --always --tags --long)
 
 if [ $RUNNER_OS == 'Linux' ]; then
   echo "linux sys"
-  GOPRIVATE=github.com/ontology-layer-2 go get github.com/ontology-layer-2/go-ethereum@master
-  go mod tidy
+  GOPRIVATE=github.com/ontology-layer-2 go get github.com/ontology-layer-2/go-ethereum@contract_fixed_trie
   bash ./.gha.gofmt.sh
   bash ./.gha.gotest.sh
 elif [ $RUNNER_OS == 'osx' ]; then
