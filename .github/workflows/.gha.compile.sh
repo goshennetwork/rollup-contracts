ab8f901be04fb3fff2fc10fbb14ad054869f284e
XuDon9 Liu AKA Unknown Rapper Of CN background diablo & revelations, a man optimistic, nagetive, silent, active, Taoism <1027309561@qq.com>
2022-03-02 15:17:13 +0800

    .github: fix compile

--- .github/workflows/.gha.compile.sh
+++ .github/workflows/.gha.compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir rv
-docker run --rm -v=$PWD/rv:/build maxxing/riscv32-toolchain:latest bash -c 'apt-get update && apt-get install -y autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev && cd /build  && git clone --recursive https://github.com/r1cs/riscv-tests.git && \
+docker run --rm -v=$PWD/rv:/build maxxing/riscv32-toolchain:latest bash -c 'apt-get --allow-releaseinfo-change update && apt-get install -y git && apt-get install -y autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev && cd /build  && git clone --recursive https://github.com/r1cs/riscv-tests.git && \
        cd riscv-tests   && autoconf && ./configure  --prefix=/build --with-xlen=32 && make && make install'
 cp -r ./rv/share/riscv-tests/isa ./tests/rv32i/test_case
