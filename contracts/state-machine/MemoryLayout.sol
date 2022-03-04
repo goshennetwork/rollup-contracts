dcdc92cda992f5ae493183b8bee35fd482f81a20
XuDon9 Liu AKA Unknown Rapper Of CN background diablo & revelations, a man optimistic, nagetive, silent, active, Taoism <1027309561@qq.com>
2022-03-04 11:47:52 +0800

    rm use less pos, fix interface

--- contracts/state-machine/MemoryLayout.sol
+++ contracts/state-machine/MemoryLayout.sol
@@ -2,42 +2,22 @@
 pragma solidity ^0.8.0;
 
 import "../interfaces/IMemory.sol";
+import "./riscv32/Register.sol";
 
 library MemoryLayout {
-    uint32 public constant InputHashPos = 0x30000000;
-    uint32 public constant HasOuputMagicPos = 0x30000800;
-    uint32 public constant OutputHashPos = 0x30000804;
-    uint32 public constant RegStartPos = 0xc0000000;
-    uint32 public constant RegZeroPos = RegStartPos;
-    uint32 public constant RegLRPos = RegStartPos + 0x1f * 4;
-    uint32 public constant RegPcPos = RegStartPos + 0x20 * 4;
-
-    uint32 public constant HasOuputMagic = 0x1337f00d;
     uint32 public constant HaltMagic = 0x5EAD0000;
 
     function readRegPC(IMemory mem, bytes32 stateHash) internal returns (uint32) {
-        return mem.read(stateHash, RegPcPos);
-    }
-
-    function writeInputHash(
-        IMemory mem,
-        bytes32 stateHash,
-        bytes32 inputHash
-    ) internal returns (bytes32) {
-        return mem.writeBytes32(stateHash, InputHashPos, inputHash);
-    }
-
-    function hasOutput(IMemory mem, bytes32 stateHash) internal returns (bool) {
-        return mem.read(stateHash, HasOuputMagicPos) == HasOuputMagic;
+        return mem.readRegister(stateHash, Register.REG_PC);
     }
 
     function isHalt(IMemory mem, bytes32 stateHash) internal returns (bool) {
-        return readRegPC(mem, stateHash) == HaltMagic;
+        return mem.readRegister(stateHash, Register.REG_PC) == HaltMagic;
     }
 
-    function readOutputRoot(IMemory mem, bytes32 stateHash) internal returns (bytes32) {
-        require(hasOutput(mem, stateHash));
-
-        return mem.readBytes32(stateHash, OutputHashPos);
+    function mustReadOutput(IMemory mem, bytes32 stateHash) internal returns (bytes32) {
+        bytes32 _out = mem.readOutput(stateHash);
+        require(_out != bytes32(0), "have no output root");
+        return _out;
     }
 }
