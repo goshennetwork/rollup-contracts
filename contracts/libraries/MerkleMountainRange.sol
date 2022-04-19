a8e84fdb90da42a1aeeb72662a0f34ff972b8faf
r1cs <1027309561@qq.com>
2022-04-19 16:31:35 +0800

    calc root

--- contracts/libraries/MerkleMountainRange.sol
+++ contracts/libraries/MerkleMountainRange.sol
@@ -9,10 +9,10 @@ library MerkleMountainRange {
         uint64 treeSize;
     }
 
-    function appendLeafHash(CompactMerkleTree storage tree, bytes32 leaf)internal {
+    function appendLeafHash(CompactMerkleTree storage tree, bytes32 leaf) internal {
         bytes32[] storage hashes = tree.hashes;
         uint64 size = uint64(tree.hashes.length);
-        for (uint s = tree.treeSize; s%2 == 1; s = s>>1) {
+        for (uint256 s = tree.treeSize; s % 2 == 1; s = s >> 1) {
             leaf = keccak256(abi.encodePacked(hashes[size - 1], leaf));
             size -= 1;
         }
@@ -22,144 +22,70 @@ library MerkleMountainRange {
             sstore(hashes.slot, size)
         }
         hashes.push(leaf);
-        tree.rootHash = bytes32(0);
+        require(hashes.length < (1 << 63), "length over flow");
+        int64 _l = int64(uint64(hashes.length));
+        bytes32 _accum = tree.hashes[uint64(_l - 1)];
+        for (int64 i = _l - 2; i >= 0; i--) {
+            _accum = keccak256(abi.encodePacked(tree.hashes[uint64(i)], _accum));
+        }
+        tree.rootHash = _accum;
     }
 
-    function verifyLeafHashInclusion(bytes32 _leafHash,uint64 _leafIndex,bytes32[] memory _proof,bytes32 _rootHash,uint64 _treeSize)internal pure {
-        require(_leafIndex<_treeSize,"leaf index out of bounds");
-        require(calculateRootHashFromAuditPath(_leafHash,_leafIndex,_proof,_treeSize)==_rootHash,"Constructed root hash differs from provided root hash");
+    function verifyLeafHashInclusion(
+        bytes32 _leafHash,
+        uint64 _leafIndex,
+        bytes32[] memory _proof,
+        bytes32 _rootHash,
+        uint64 _treeSize
+    ) internal pure {
+        require(_leafIndex < _treeSize, "leaf index out of bounds");
+        require(
+            calculateRootHashFromAuditPath(_leafHash, _leafIndex, _proof, _treeSize) == _rootHash,
+            "Constructed root hash differs from provided root hash"
+        );
     }
 
-    function calculateRootHashFromAuditPath(bytes32 _leafHash,uint64 _leafIndex,bytes32[] memory _auditPath,uint64 _treeSize)internal pure returns(bytes32){
+    function calculateRootHashFromAuditPath(
+        bytes32 _leafHash,
+        uint64 _leafIndex,
+        bytes32[] memory _auditPath,
+        uint64 _treeSize
+    ) internal pure returns (bytes32) {
         bytes32 _calculatedHash = _leafHash;
         uint64 _pos;
-        uint64 _pathLen=uint64(_auditPath.length);
-        for (uint64 _lastNode = _treeSize-1;_lastNode>0;_lastNode>>=1){
-        require(_pos < _pathLen,"proof too short");
-            if (_leafIndex%2==1){
-                _calculatedHash=keccak256(abi.encodePacked(_auditPath[_pos],_calculatedHash));
-            }else if (_leafIndex<_lastNode){
-                _calculatedHash=keccak256(abi.encodePacked(_calculatedHash,_auditPath[_pos]));
+        uint64 _pathLen = uint64(_auditPath.length);
+        for (uint64 _lastNode = _treeSize - 1; _lastNode > 0; _lastNode >>= 1) {
+            require(_pos < _pathLen, "proof too short");
+            if (_leafIndex % 2 == 1) {
+                _calculatedHash = keccak256(abi.encodePacked(_auditPath[_pos], _calculatedHash));
+            } else if (_leafIndex < _lastNode) {
+                _calculatedHash = keccak256(abi.encodePacked(_calculatedHash, _auditPath[_pos]));
             }
             _pos++;
-            _leafIndex>>=1;
+            _leafIndex >>= 1;
         }
-        require(_pos>=_pathLen,"proof too long");
+        require(_pos >= _pathLen, "proof too long");
         return _calculatedHash;
     }
 
-    struct RootNode {
-        uint64 level;
-        bytes32 hash;
-    }
-
-    /**
-     * @dev Append a leaf to a list of perfect binary tree
-     * @param _trees A list of trees' root, ranging from highest level to lowest level, including root level,root hash
-     * @param _hash New leaf(level 0)
-     */
-    function appendLeafHash(RootNode[] memory _trees, bytes32 _hash) internal pure returns (RootNode[] memory) {
-        RootNode[] memory _arrs = new RootNode[](_trees.length + 1);
-        for (uint256 i = 0; i < _trees.length; i++) {
-            _arrs[i] = _trees[i];
+    //@dev hash child tree add prefix byte(1) to data
+    function merkleRoot(CompactMerkleTree storage tree) internal returns (bytes32) {
+        if (tree.rootHash != bytes32(0)) {
+            //not changed
+            return tree.rootHash;
         }
-        _arrs[_trees.length] = RootNode({ level: 0, hash: _hash });
-        uint256 _len = _arrs.length;
-        uint256 i = _len - 1;
-        for (; i > 0; i--) {
-            if (_arrs[i].level == _arrs[i - 1].level) {
-                //merge
-                _trees[i - 1].level += 1;
-                _trees[i - 1].hash = keccak256(abi.encodePacked(_arrs[i - 1].hash, _arrs[i].hash));
-            } else {
-                break;
-            }
+        if (tree.treeSize == 0) {
+            //no leaf,simply return empty hash
+            return bytes32(0);
         }
-        _len = i + 1;
-        assembly {
-            //resize
-            mstore(_arrs, _len)
+        //changed,re calc
+        require(tree.hashes.length < (1 << 63), "length over flow");
+        int64 _l = int64(uint64(tree.hashes.length));
+        assert(_l >= 0);
+        bytes32 _accum = tree.hashes[uint64(_l - 1)];
+        for (int64 i = _l - 2; i >= 0; i--) {
+            _accum = keccak256(abi.encodePacked(tree.hashes[uint64(i)], _accum));
         }
-        return _arrs;
-    }
-
-    /**
-     * @dev Verify a list of ranged trees is right///maybe do not need it, locally store a set of trees
-     */
-    function verifyTrees(bytes32 _root, RootNode[] memory _trees) internal pure returns (bool) {
-        return genMMRRoot(_trees) == _root;
-    }
-
-    /**
-     * @dev Chose a tree from a list of ranged trees by leaf index
-     * @param _trees A list of ranged trees
-     * @param _index Leaf index
-     * @return _treeIndex Tree index in list of range trees, which contains wanted leaf
-     * @return _preTotal Previous trees have stored total number of leaves
-     * @notice revert if find noting (wanted leaf beyond local record)
-     */
-    function chooseTreeNode(RootNode[] memory _trees, uint64 _index)
-        internal
-        pure
-        returns (uint64 _treeIndex, uint64 _preTotal)
-    {
-        uint64 _num;
-        for (uint64 i = 0; i < _trees.length; i++) {
-            uint64 _pre = _num;
-            _num += uint64(1) << _trees[i].level;
-            if (_num > _index) {
-                return (i, _pre);
-            }
-        }
-        revert("find nothing");
-    }
-
-    /**
-     * @dev Verify leaf in a perfect binary tree
-     * @param _siblings Leaf node's sibling to generate merkle tree root,ranging from low level to highest level(except root)
-     * @param _treeRoot Root of this tree
-     * @param _index Leaf index in this tree
-     * @param _totalLeaf Total leaf in this tree,because this tree is perfect binary tree, _totalLeaf must be 2^n
-     * @param _leaf Leaf node
-     * @notice revert if:
-     * - have no leaf
-     * - leaf node's index beyond tree capcity
-     * - provided sibling num not equal to (log2(total leaf num in tree))
-     */
-    function verifyLeafTree(
-        bytes32[] memory _siblings,
-        bytes32 _treeRoot,
-        uint64 _index,
-        uint64 _totalLeaf,
-        bytes32 _leaf
-    ) internal pure returns (bool) {
-        require(_totalLeaf > 0, "Total leaves must be greater than zero.");
-        require(_index < _totalLeaf, "Index out of bounds.");
-        bytes32 _computedRoot = _leaf;
-        uint64 _calculatedNum = 1;
-        for (uint256 i = 0; i < _siblings.length; i++) {
-            if ((_index & 1) == 1) {
-                _computedRoot = keccak256(abi.encodePacked(_siblings[i], _computedRoot));
-            } else {
-                _computedRoot = keccak256(abi.encodePacked(_computedRoot, _siblings[i]));
-            }
-            _calculatedNum <<= 1;
-            _index >>= 1;
-        }
-        require(_calculatedNum == _totalLeaf, "calculated num should equal to total leaf");
-        return _computedRoot == _treeRoot;
-    }
-
-    /**
-     * @dev Gen merkle moutain tree roots.It simply hash one by one
-     * @param _trees A list of ranged tree root node
-     */
-    function genMMRRoot(RootNode[] memory _trees) internal pure returns (bytes32) {
-        require(_trees.length > 0, "no tree");
-        bytes32 _left = _trees[0].hash;
-        for (uint256 i = 1; i < _trees.length; i++) {
-            _left = keccak256(abi.encodePacked(_left, _trees[i].hash));
-        }
-        return _left;
+        return _accum;
     }
 }
--- contracts/libraries/MerkleMountainRange.t.sol
+++ contracts/libraries/MerkleMountainRange.t.sol
@@ -3,118 +3,112 @@ pragma solidity ^0.8.0;
 
 import "./MerkleMountainRange.sol";
 import "./console.sol";
+import "./MerkleMountainRange.sol";
 
 contract MMRTest {
+    using MerkleMountainRange for MerkleMountainRange.CompactMerkleTree;
+    MerkleMountainRange.CompactMerkleTree _trees;
+
+    function getTreeSize() public returns (uint64) {
+        return _trees.treeSize;
+    }
+
+    function getRootHash() public returns (bytes32) {
+        return _trees.merkleRoot();
+    }
+
+    function verifyProof(
+        bytes32 _leafHash,
+        uint64 _leafIndex,
+        bytes32[] memory _proof,
+        bytes32 _rootHash,
+        uint64 _treeSize
+    ) public {
+        MerkleMountainRange.verifyLeafHashInclusion(_leafHash, _leafIndex, _proof, _rootHash, _treeSize);
+    }
+
     function testAppend() public {
-        MerkleMountainRange.RootNode[] memory _trees;
         uint256 _total;
         _total++;
-        _trees = MerkleMountainRange.appendLeafHash(_trees, bytes32(0));
-        console.logUint(_trees.length); ///1
+        MerkleMountainRange.appendLeafHash(_trees, bytes32(0));
+        console.logUint(_trees.hashes.length); ///1
         _total++;
-        _trees = MerkleMountainRange.appendLeafHash(_trees, bytes32(0));
-        console.logUint(_trees.length); ///1
+        MerkleMountainRange.appendLeafHash(_trees, bytes32(0));
+        console.logUint(_trees.hashes.length); ///1
         _total++;
-        _trees = MerkleMountainRange.appendLeafHash(_trees, bytes32(0));
-        console.logUint(_trees.length); ///2
+        MerkleMountainRange.appendLeafHash(_trees, bytes32(0));
+        console.logUint(_trees.hashes.length); ///2
         _total++;
-        _trees = MerkleMountainRange.appendLeafHash(_trees, bytes32(0));
-        console.logUint(_trees.length); ///1
+        MerkleMountainRange.appendLeafHash(_trees, bytes32(0));
+        console.logUint(_trees.hashes.length); //1
         _total++;
-        _trees = MerkleMountainRange.appendLeafHash(_trees, bytes32(0));
-        console.logUint(_trees.length); ///2
+        MerkleMountainRange.appendLeafHash(_trees, bytes32(0));
+        console.logUint(_trees.hashes.length); ///2
         _total++;
-        _trees = MerkleMountainRange.appendLeafHash(_trees, bytes32(0));
-        console.logUint(_trees.length); ///2
+        MerkleMountainRange.appendLeafHash(_trees, bytes32(0));
+        console.logUint(_trees.hashes.length); ///2
         _total++;
-        _trees = MerkleMountainRange.appendLeafHash(_trees, bytes32(0));
-        console.logUint(_trees.length); ///3\
-
-        uint64 indexInTrees;
-        uint64 preTotalNum;
-        (indexInTrees, preTotalNum) = MerkleMountainRange.chooseTreeNode(_trees, 0);
-        require(indexInTrees == 0 && preTotalNum == 0, "0");
-        (indexInTrees, preTotalNum) = MerkleMountainRange.chooseTreeNode(_trees, 1);
-        require(indexInTrees == 0 && preTotalNum == 0, "1");
-        (indexInTrees, preTotalNum) = MerkleMountainRange.chooseTreeNode(_trees, 2);
-        require(indexInTrees == 0 && preTotalNum == 0, "2");
-        (indexInTrees, preTotalNum) = MerkleMountainRange.chooseTreeNode(_trees, 3);
-        require(indexInTrees == 0 && preTotalNum == 0, "3");
-        (indexInTrees, preTotalNum) = MerkleMountainRange.chooseTreeNode(_trees, 4);
-        require(indexInTrees == 1 && preTotalNum == 4, "4");
-        (indexInTrees, preTotalNum) = MerkleMountainRange.chooseTreeNode(_trees, 5);
-        require(indexInTrees == 1 && preTotalNum == 4, "4");
-        (indexInTrees, preTotalNum) = MerkleMountainRange.chooseTreeNode(_trees, 6);
-        require(indexInTrees == 2 && preTotalNum == 6, "6");
+        MerkleMountainRange.appendLeafHash(_trees, bytes32(0));
+        console.logUint(_trees.hashes.length); ///3\
     }
 
-    function append7Leaf() internal returns (MerkleMountainRange.RootNode[] memory) {
-        MerkleMountainRange.RootNode[] memory _trees;
+    function append7Leaf() internal {
+        MerkleMountainRange.appendLeafHash(_trees, bytes32(0));
 
-        _trees = MerkleMountainRange.appendLeafHash(_trees, bytes32(0));
-        console.logUint(_trees.length); ///1
+        MerkleMountainRange.appendLeafHash(_trees, bytes32(uint256(1)));
 
-        _trees = MerkleMountainRange.appendLeafHash(_trees, bytes32(uint256(1)));
-        console.logUint(_trees.length); ///1
+        MerkleMountainRange.appendLeafHash(_trees, bytes32(uint256(2)));
 
-        _trees = MerkleMountainRange.appendLeafHash(_trees, bytes32(uint256(2)));
-        console.logUint(_trees.length); ///2
+        MerkleMountainRange.appendLeafHash(_trees, bytes32(uint256(3)));
 
-        _trees = MerkleMountainRange.appendLeafHash(_trees, bytes32(uint256(3)));
-        console.logUint(_trees.length); ///1
+        MerkleMountainRange.appendLeafHash(_trees, bytes32(uint256(4)));
 
-        _trees = MerkleMountainRange.appendLeafHash(_trees, bytes32(uint256(4)));
-        console.logUint(_trees.length); ///2
+        MerkleMountainRange.appendLeafHash(_trees, bytes32(uint256(5)));
 
-        _trees = MerkleMountainRange.appendLeafHash(_trees, bytes32(uint256(5)));
-        console.logUint(_trees.length); ///2
-
-        _trees = MerkleMountainRange.appendLeafHash(_trees, bytes32(uint256(6)));
-        console.logUint(_trees.length); ///3\
-        return _trees;
+        MerkleMountainRange.appendLeafHash(_trees, bytes32(uint256(6)));
     }
 
-    function testGenRoot() public {
-        MerkleMountainRange.RootNode[] memory _trees = append7Leaf();
-        bytes32 _root = MerkleMountainRange.genMMRRoot(_trees);
-        require(MerkleMountainRange.verifyTrees(_root, _trees), "verify");
-    }
-
-    function testverifyLeafTree() public {
-        MerkleMountainRange.RootNode[] memory _trees = append7Leaf();
-
-        ///index 2
-        uint64 _indexInAll = 2;
-        uint64 indexInTrees;
-        uint64 preTotalNum;
-        (indexInTrees, preTotalNum) = MerkleMountainRange.chooseTreeNode(_trees, _indexInAll);
-
-        bytes32[] memory _siblings = new bytes32[](2);
-        _siblings[0] = bytes32(uint256(3));
-        _siblings[1] = keccak256(abi.encodePacked(bytes32(0), bytes32(uint256(1))));
-        bool _correct = MerkleMountainRange.verifyLeafTree(
-            _siblings,
-            _trees[indexInTrees].hash,
-            _indexInAll - preTotalNum,
-            uint64(1) << _trees[indexInTrees].level,
-            bytes32(uint256(2))
-        );
-        require(_correct, "index 2");
-
-        //index 6
-        _indexInAll = 6;
-        (indexInTrees, preTotalNum) = MerkleMountainRange.chooseTreeNode(_trees, _indexInAll);
-        assembly {
-            //do not need siblings
-            mstore(_siblings, 0)
-        }
-        _correct = MerkleMountainRange.verifyLeafTree(
-            _siblings,
-            _trees[indexInTrees].hash,
-            _indexInAll - preTotalNum,
-            uint64(1) << _trees[indexInTrees].level,
-            bytes32(uint256(6))
-        );
-        require(_correct, "index 6");
-    }
+    //    function testGenRoot() public {
+    //        MerkleMountainRange.RootNode[] memory _trees = append7Leaf();
+    //        bytes32 _root = MerkleMountainRange.genMMRRoot(_trees);
+    //        require(MerkleMountainRange.verifyTrees(_root, _trees), "verify");
+    //    }
+    //
+    //    function testverifyLeafTree() public {
+    //        MerkleMountainRange.RootNode[] memory _trees = append7Leaf();
+    //
+    //        ///index 2
+    //        uint64 _indexInAll = 2;
+    //        uint64 indexInTrees;
+    //        uint64 preTotalNum;
+    //        (indexInTrees, preTotalNum) = MerkleMountainRange.chooseTreeNode(_trees, _indexInAll);
+    //
+    //        bytes32[] memory _siblings = new bytes32[](2);
+    //        _siblings[0] = bytes32(uint256(3));
+    //        _siblings[1] = keccak256(abi.encodePacked(bytes32(0), bytes32(uint256(1))));
+    //        bool _correct = MerkleMountainRange.verifyLeafTree(
+    //            _siblings,
+    //            _trees[indexInTrees].hash,
+    //            _indexInAll - preTotalNum,
+    //            uint64(1) << _trees[indexInTrees].level,
+    //            bytes32(uint256(2))
+    //        );
+    //        require(_correct, "index 2");
+    //
+    //        //index 6
+    //        _indexInAll = 6;
+    //        (indexInTrees, preTotalNum) = MerkleMountainRange.chooseTreeNode(_trees, _indexInAll);
+    //        assembly {
+    //            //do not need siblings
+    //            mstore(_siblings, 0)
+    //        }
+    //        _correct = MerkleMountainRange.verifyLeafTree(
+    //            _siblings,
+    //            _trees[indexInTrees].hash,
+    //            _indexInAll - preTotalNum,
+    //            uint64(1) << _trees[indexInTrees].level,
+    //            bytes32(uint256(6))
+    //        );
+    //        require(_correct, "index 6");
+    //    }
 }
