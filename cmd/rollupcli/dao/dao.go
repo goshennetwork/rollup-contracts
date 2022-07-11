5eab4138b5342a4827654e69cef9d709972f5ad4
XuDon9 Liu AKA Unknown Rapper Of CN background diablo & revelations, a man optimistic, nagetive, silent, active, Taoism <1027309561@qq.com>
2022-07-11 10:53:37 +0800

    add proposer

--- cmd/rollupcli/dao/dao.go
+++ cmd/rollupcli/dao/dao.go
@@ -33,26 +33,75 @@ func SubCommand() []*cli.Command {
 			},
 			Action: sequencerWhitelist,
 		},
+		{
+			Name:  "proposerWhitelist",
+			Usage: "set proposer whitelist",
+			Flags: []cli.Flag{
+				flags.AccountFlag,
+				flags.EnabledFlag,
+				flags.SubmitFlag,
+			},
+			Action: proposerWhitelist,
+		},
 	}
 }
 
 func sequencerWhitelist(ctx *cli.Context) error {
+	params, err := parseCtx(ctx)
+	if err != nil {
+		return err
+	}
+	setSequencerWhitelist(params)
+	return nil
+}
+
+func proposerWhitelist(ctx *cli.Context) error {
+	params, err := parseCtx(ctx)
+	if err != nil {
+		return err
+	}
+	setProposerWhitelist(params)
+	return nil
+}
+
+type param struct {
+	signer  *contract.Signer
+	dao     web3.Address
+	target  web3.Address
+	enabled bool
+}
+
+func parseCtx(ctx *cli.Context) (*param, error) {
 	path := ctx.String(flags.ConfigFlag.Name)
 	signer, conf, err := common.SetUpL1(path)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	acc := ctx.String(flags.AccountFlag.Name)
 	enabled := ctx.Bool(flags.EnabledFlag.Name)
 	submit := ctx.Bool(flags.SubmitFlag.Name)
 	signer.Submit = submit
-	setSequencerWhitelist(signer, conf.L1Addresses.DAO, web3.HexToAddress(acc), enabled)
-	return nil
+	return &param{signer, conf.L1Addresses.DAO, web3.HexToAddress(acc), enabled}, nil
 }
 
-func setSequencerWhitelist(signer *contract.Signer, dao web3.Address, sequencer web3.Address, enabled bool) {
+func setSequencerWhitelist(params *param) {
+	dao := params.dao
+	signer := params.signer
+	sequencer := params.target
+	enabled := params.enabled
 	c := binding.NewDAO(dao, signer.Client)
 	c.Contract().SetFrom(signer.Address())
 	receipt := c.SetSequencerWhitelist(sequencer, enabled).Sign(signer).SendTransaction(signer).EnsureNoRevert()
 	log.Info("set sequencer whitelist", "dao", dao, "sequencer", sequencer, "enabled", enabled, "receipt", utils.JsonStr(receipt))
+}
+
+func setProposerWhitelist(params *param) {
+	dao := params.dao
+	signer := params.signer
+	proposer := params.target
+	enabled := params.enabled
+	c := binding.NewDAO(dao, signer.Client)
+	c.Contract().SetFrom(signer.Address())
+	receipt := c.SetProposerWhitelist(proposer, enabled).Sign(signer).SendTransaction(signer).EnsureNoRevert()
+	log.Info("set proposer whitelist", "dao", dao, "proposer", proposer, "enabled", enabled, "receipt", utils.JsonStr(receipt))
 }
--- doc/sequencer.md
+++ doc/sequencer.md
@@ -2,7 +2,7 @@
 
 一个可以对RollupInputChain合约进行appendBatch操作的账户即为`sequencer`,其需要两个权限:
 - 需要在`DAO`的白名单内，以便在现阶段保证足够的安全性
-- 需要在`StakingManager`中进行抵押，以便对欺诈的sequencer进行经济惩罚
+- 需要在`StakingManager`中进行抵押，以防女巫攻击
 
 设置权限的步骤:
 1: 账户A需要保证在`FeeToken`ERC20合约中有足够的可用于后续操作的token（数额可查询`StakingManager`的`price()`方法）。
