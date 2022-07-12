b0e91b95dee93a5516ac6d73816bd1ffdd227cf5
laizy <aochyi@126.com>
2022-07-12 17:52:06 +0800

    only check contract api changes

--- .github/workflows/.gha.binding.sh
+++ .github/workflows/.gha.binding.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-diff <(git diff ./binding) <(echo -n)
+# note: yarn abigen already executed in go.yml
+# fixme: we only check the contract api changes here
+# shellcheck disable=SC2010
+diff <(ls ./binding/*.go | grep --invert-match "artifacts" | xargs git diff) <(echo -n)
