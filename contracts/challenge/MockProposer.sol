6fa11cf69576448365e616a1feedf1892c0a1072
sole12343 <m18640126583@163.com>
2022-07-20 14:46:38 +0800

    test Challenge.sol

---
+++ contracts/challenge/MockProposer.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.8.0;
+
+import "../interfaces/IChallenge.sol";
+import "../interfaces/IStakingManager.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MockProposer {
+    IChallenge challenge;
+    IStakingManager sm;
+
+    function setChallenge(IChallenge _c) external {
+        challenge = _c;
+    }
+
+    function initialize(uint64 _endStep, bytes32 _midSystemState) external {
+        challenge.initialize(_endStep, bytes32(uint256(0x6666)), _midSystemState);
+    }
+
+    function revealMidStates(uint256[] calldata _nodeKeys, bytes32[] calldata _stateRoots) external {
+        challenge.revealMidStates(_nodeKeys, _stateRoots);
+    }
+
+    function approve(
+        IERC20 _token,
+        address _spender,
+        uint256 _amount
+    ) external {
+        _token.approve(_spender, _amount);
+    }
+
+    ///staking manager action of proposer
+    function setStakingManager(IStakingManager _s) external {
+        sm = _s;
+    }
+
+    function deposit() public {
+        sm.deposit();
+    }
+
+    function startWithdrawal() public {
+        sm.startWithdrawal();
+    }
+
+    // function finalizeWithdrawal()public{
+    //     sm.finalizeWithdrawal();
+    // }
+}
