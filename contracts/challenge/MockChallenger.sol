1d0b7bed694fef51c039e8b9ae77fcdf38f5908d
sole12343 <m18640126583@163.com>
2022-07-28 16:20:55 +0800

    test challenge

---
+++ contracts/challenge/MockChallenger.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.8.0;
+
+import "../interfaces/IChallenge.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../interfaces/IStakingManager.sol";
+
+contract MockChallenger {
+    IChallenge challenge;
+    IStakingManager sm;
+
+    function setChallenge(IChallenge _c) external {
+        challenge = _c;
+    }
+
+    function selectDisputeBranch(uint256[] calldata _parentNodeKey, bool[] calldata _isLeft) external {
+        challenge.selectDisputeBranch(_parentNodeKey, _isLeft);
+    }
+
+    // function claimChallengerWin(address _challenger) external {
+    //     challenge.claimChallengerWin(_challenger);
+    // }
+
+    function approve(
+        IERC20 _token,
+        address _spender,
+        uint256 _amount
+    ) external {
+        _token.approve(_spender, _amount);
+    }
+}
