64c4414e8801988e6aea06daaa29e2dba1cd2edd
qiluge <19525062+qiluge@users.noreply.github.com>
2022-09-05 16:57:54 +0800

    add some comments

--- contracts/test-helper/PreCompile.t.sol
+++ contracts/test-helper/PreCompile.t.sol
@@ -10,6 +10,7 @@ contract TestPreCompile {
 
     PreCompile public precompile;
 
+    // 0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199
     uint256 private privKey = 0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e;
 
     function setUp() public {
