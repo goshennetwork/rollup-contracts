03857d1f09ef0fdc2fef773a28964e324ca95c04
lucas7788 <sishsh@163.com>
2022-09-13 18:06:48 +0800

    Delete redundant events

--- contracts/token/L2StandardERC20.sol
+++ contracts/token/L2StandardERC20.sol
@@ -26,24 +26,21 @@ contract L2StandardERC20 is IL2StandardERC20, ERC20 {
 
     // slither-disable-next-line external-function
     function supportsInterface(bytes4 _interfaceId) public pure returns (bool) {
-        bytes4 firstSupportedInterface = bytes4(keccak256("supportsInterface(bytes4)")); // ERC165
+        bytes4 firstSupportedInterface = bytes4(keccak256("supportsInterface(bytes4)"));
+        // ERC165
         bytes4 secondSupportedInterface = IL2StandardERC20.l1Token.selector ^
-            IL2StandardERC20.mint.selector ^
-            IL2StandardERC20.burn.selector;
+        IL2StandardERC20.mint.selector ^
+        IL2StandardERC20.burn.selector;
         return _interfaceId == firstSupportedInterface || _interfaceId == secondSupportedInterface;
     }
 
     // slither-disable-next-line external-function
     function mint(address _to, uint256 _amount) public virtual onlyL2Bridge {
         _mint(_to, _amount);
-
-        emit Transfer(address(0), _to, _amount);
     }
 
     // slither-disable-next-line external-function
     function burn(address _from, uint256 _amount) public virtual onlyL2Bridge {
         _burn(_from, _amount);
-
-        emit Transfer(_from, address(0), _amount);
     }
 }
