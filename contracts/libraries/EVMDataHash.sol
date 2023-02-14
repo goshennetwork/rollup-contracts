d7cab742ac4fc1238011bb49bb10267bccac8b10
f <f>
2023-02-14 16:27:55 +0800

    RollupInputChain: blob supported

---
+++ contracts/libraries/EVMDataHash.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library EVMDataHash {
+    function datahash(uint256 _index) internal returns (bytes32) {
+        revert("no imp");
+    }
+}
