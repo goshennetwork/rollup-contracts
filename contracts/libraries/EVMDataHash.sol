bfec713f11fb26ad1a8e5fadb60951bc09b56671
f <f>
2023-02-14 16:35:26 +0800

    add fake version hash

--- contracts/libraries/EVMDataHash.sol
+++ contracts/libraries/EVMDataHash.sol
@@ -2,7 +2,12 @@
 pragma solidity ^0.8.0;
 
 library EVMDataHash {
+    bytes32 constant FAKE_VERSION_HASH_LIST_0 = bytes32(uint256(0xffff));
+
     function datahash(uint256 _index) internal returns (bytes32) {
-        revert("no imp");
+        if (_index == 0) {
+            return FAKE_VERSION_HASH_LIST_0;
+        }
+        return bytes32(0);
     }
 }
