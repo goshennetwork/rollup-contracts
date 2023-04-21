2df7bb2fcefb71d1da682d05deb38b33133bdea0
f <f>
2023-04-21 14:31:43 +0800

    abigen gen chosen files

--- scripts/abigen.bash
+++ scripts/abigen.bash
@@ -1,26 +1,26 @@
 set -ex
 
 files="
-AddressManager.sol/*
-ChainStorageContainer.sol/*
-Challenge.sol/*
-ERC20.sol/*
-L1CrossLayerWitness.sol/*
-L2CrossLayerWitness.sol/*
-L1StandardBridge.sol/*
-L2StandardBridge.sol/*
-RollupInputChain.sol/*
-RollupStateChain.sol/*
-StakingManager.sol/*
-TestERC20.sol/*
-ChallengeFactory.sol/*
-UpgradeableBeacon.sol/*
-ProxyAdmin.sol/*
-TransparentUpgradeableProxy.sol/*
-Whitelist.sol/*
-L2FeeCollector.sol/*
-TestL2ERC20.sol/*
-L2StandardERC20.sol/*
+AddressManager.sol/AddressManager.json
+ChainStorageContainer.sol/ChainStorageContainer.json
+Challenge.sol/Challenge.json
+ERC20.sol/ERC20.json
+L1CrossLayerWitness.sol/L1CrossLayerWitness.json
+L2CrossLayerWitness.sol/L2CrossLayerWitness.json
+L1StandardBridge.sol/L1StandardBridge.json
+L2StandardBridge.sol/L2StandardBridge.json
+RollupInputChain.sol/RollupInputChain.json
+RollupStateChain.sol/RollupStateChain.json
+StakingManager.sol/StakingManager.json
+TestERC20.sol/TestERC20json
+ChallengeFactory.sol/ChallengeFactory.json
+UpgradeableBeacon.sol/UpgradeableBeacon.json
+ProxyAdmin.sol/ProxyAdmin.json
+TransparentUpgradeableProxy.sol/TransparentUpgradeableProxy.json
+Whitelist.sol/.json
+L2FeeCollector.sol/L2FeeCollector.json
+TestL2ERC20.sol/TestL2ERC20.json
+L2StandardERC20.sol/L2StandardERC20.json
 "
 prefix="out/"
 for file in $files ; do
