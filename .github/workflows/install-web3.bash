4a4dadf71149e506363139c254de775da0de79b4
f <f>
2023-04-26 11:32:57 +0800

    install newest go-web3

--- .github/workflows/install-web3.bash
+++ .github/workflows/install-web3.bash
@@ -1,4 +1,4 @@
 
-go install github.com/laizy/web3/abigen/cmd@v0.1.9
+go install github.com/laizy/web3/abigen/cmd@719f161a0ed18baa58322fbb0841cfa73c0f3cc8
 
 mv $(go env GOBIN)/cmd $(go env GOBIN)/go-web3
